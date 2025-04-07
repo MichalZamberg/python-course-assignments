6ced13219e3b2a23a5bb930b8631a0d7207bbda5
MichalZamberg <michal.zamberg-elad@weizmann.ac.il>
2025-04-07 10:20:53 +0300

    Update circle.py

--- day02/circle.py
+++ day02/circle.py
@@ -1,1 +1,10 @@
+import numpy as np
+
+r = float(input("Hi User, Please enter the radius of your choice: "))
+
+area = np.pi * r ** 2
+circum = 2 * np.pi * r
+
+print(f"Area of the circle: {area:.2f}")
+print(f"Circumference of the circle: {circum:.2f}")
 
