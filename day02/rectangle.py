4682ce09a31a53ca05549d40e11bed7bce016908
MichalZamberg <michal.zamberg-elad@weizmann.ac.il>
2025-04-07 10:24:09 +0300

    Create rectangle.py

---
+++ day02/rectangle.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+h=float(input("Hi User, Please choose a heigt for your rectangle: "))
+w=float(input("Hi User, Please choose a width for your rectangle: "))
+
+area=h*w
+perimeter=2*h+2*w
+
+print(f"The area of the rectangle: {area:.2f}")
+print(f"The perimeter of the circle: {perimeter:.2f}")
