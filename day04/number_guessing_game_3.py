5dae236e24165ba92f97f8881b70c4489b975004
MichalZamberg <michal.zamberg-elad@weizmann.ac.il>
2025-05-12 09:41:26 +0300

    Create number_guessing_game_3.py

---
+++ day04/number_guessing_game_3.py
@@ -0,0 +1,25 @@
+import random
+
+secret_number = random.randint(1, 20)
+print("I'm thinking of a number between 1 and 20.")
+print("Type 'x' to exit the game at any time.")
+print("Type 's' to reveal the secret number (cheat).")
+
+while True:
+    guess = input("Take a guess: ")
+    if guess.lower() == 'x':
+        print("You chose to exit the game. Goodbye!")
+        break
+    elif guess.lower() == 's':
+        print(f"(Cheat) The secret number is: {secret_number}")
+        continue
+    try:
+        guess = int(guess)
+        if guess < secret_number:
+            print("Try again.")
+        elif guess > secret_number:
+            print("Try again.")
+        else:
+            print("You guessed the number!")
+            break
+
