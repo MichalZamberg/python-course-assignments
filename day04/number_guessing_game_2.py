d1097b6218859495c8d023666e9a00fe25b816cc
MichalZamberg <michal.zamberg-elad@weizmann.ac.il>
2025-05-12 09:58:00 +0300

    Update number_guessing_game_2.py

--- day04/number_guessing_game_2.py
+++ day04/number_guessing_game_2.py
@@ -1,7 +1,7 @@
 import random
 
-secret_number = random.randint(1, 100)
-print("I'm thinking of a number between 1 and 100.")
+secret_number = random.randint(1, 20)
+print("I'm thinking of a number between 1 and 20.")
 print("Type 'x' to exit the game at any time.")
 
 while True:
@@ -9,13 +9,13 @@ while True:
     if guess.lower() == 'x':
         print("You chose to exit the game. Goodbye!")
         break
-    try:
-        guess = int(guess)
-        if guess < secret_number:
-            print("Try again.")
-        elif guess > secret_number:
-            print("Try again.")
-        else:
-            print("You guessed the number!")
-            break
+
+    guess = int(guess)
+    if guess < secret_number:
+        print("Too low. Try again.")
+    elif guess > secret_number:
+        print("Too high. Try again.")
+    else:
+        print("You guessed the number!")
+        break
 
